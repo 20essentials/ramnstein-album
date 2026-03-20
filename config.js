0d8b4e2de0258d9faeb2b69bfadd525d31777b03
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:12:41 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RAMMSTEIN';
+export let nameSongs = [
+	"DU HAST", /*1*/
+	"ENGEL", /*2*/
+	"SONNE", /*3*/
+	"MEIN TEIL", /*4*/
+	"ICH WILL", /*5*/
+	"FEUER FREI!", /*6*/
+	"KEINE LUST", /*7*/
+	"DU RIECHST SO GUT", /*8*/
+	"MUTTER", /*9*/
+	"SEEMANN", /*10*/
+	"AMERIKA", /*11*/
+	"WO BIST DU", /*12*/
+	"SPRING", /*13*/
+	"HILF MIR", /*14*/
+	"ZERSTÖREN", /*15*/
+	"BENZIN", /*16*/
+	"MANN GEGEN MANN", /*17*/
+	"MEIN LAND", /*18*/
+	"DALAI LAMA", /*19*/
+	"MORGENSTERN", /*20*/
+]
